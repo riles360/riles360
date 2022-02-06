c94a0f1a24c6b135695783c090ae02c2c9e8a972
riles360 <62363324+riles360@users.noreply.github.com>
2022-02-06 07:32:43 -0400

    Add files via upload

---
+++ package_loading.R
@@ -0,0 +1,5 @@
+my_packages <- ("devtools")
+#install.packages("rlang")
+install.packages(my_packages, repos = "http://cran.rstudio.com")
+
+# my_packages <- c("tidyverse","broom","coefplot")#,"cowplot","gapminder","GGally","ggrepel","ggridges","gridExtra","here","interplot","margins","maps","mapproj","mapdata","MASS","quantreg","rlang","scales","survey","srvyr","viridis","viridisLite","devtools")
